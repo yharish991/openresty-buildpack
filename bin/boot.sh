3ad4b971e9181cdd2ac3d15e354a21476681c0ab
hyayi <hyayi@us.ibm.com>
2017-09-11 16:17:45 -0400

    new version of openresty buildpack

--- bin/boot.sh
+++ bin/boot.sh
@@ -1,27 +1,21 @@
 export APP_ROOT=$HOME
-
-export LD_LIBRARY_PATH=$APP_ROOT/openresty/luajit/lib:$LD_LIBRARY_PATH
-
-export PATH=$APP_ROOT/openresty/bin:$PATH
-
-mkdir -p /tmp/staged/app/openresty/nginx/logs
-touch /tmp/staged/app/openresty/nginx/logs/error.log
-mkdir -p /tmp/staged/app/openresty/nginx/client_body_temp
+export LD_LIBRARY_PATH=$APP_ROOT/openresty/lib:$LD_LIBRARY_PATH
 
 mkdir -p $APP_ROOT/nginx/logs
 touch $APP_ROOT/nginx/logs/error.log
 touch $APP_ROOT/nginx/logs/access.log
 
-cp -a $APP_ROOT/openresty/lualib /tmp/staged/app/openresty/lualib/
-
 conf_file=$APP_ROOT/openresty/nginx/conf/nginx.conf
-if [ -f $APP_ROOT/scripts/nginx.conf ]
-then
-  conf_file=$APP_ROOT/scripts/nginx.conf
-fi
 
-mv $conf_file $APP_ROOT/openresty/nginx/conf/orig.conf
-erb $APP_ROOT/openresty/nginx/conf/orig.conf > $APP_ROOT/openresty/nginx/conf/nginx.conf
+erb $conf_file > $APP_ROOT/openresty/nginx/conf/nginx-final.conf
 
-# start nginx
-exec $APP_ROOT/openresty/nginx/sbin/nginx -c $APP_ROOT/openresty/nginx/conf/nginx.conf
+# ------------------------------------------------------------------------------------------------
+
+mkfifo $APP_ROOT/openresty/nginx/logs/access.log
+mkfifo $APP_ROOT/openresty/nginx/logs/error.log
+
+cat < $APP_ROOT/openresty/nginx/logs/access.log &
+(>&2 cat) < $APP_ROOT/openresty/nginx/logs/error.log &
+
+exec $APP_ROOT/openresty/nginx/sbin/nginx -p $APP_ROOT/openresty/nginx -c $APP_ROOT/openresty/nginx/conf/nginx-final.conf
+# ------------------------------------------------------------------------------------------------
